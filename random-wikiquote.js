fc4f4bdd1bbb85edfab2043d1e650d1facfaa970
unknown <samuel_newhouse@yahoo.com>
2019-04-04 08:23:16 -0500

    Cleanup and simplify the module

--- random-wikiquote.js
+++ random-wikiquote.js
@@ -1,3 +1,15 @@
+const RandomWikiquote = {};
+const BASE_URL = "https://en.wikiquote.org/w/api.php?origin=*&format=json";
+const RETRY_LIMIT = 7;
+
+let minLength = 20;
+let maxLength = 300;
+let numericLimit = 0.1;
+
+RandomWikiquote.setMinLength = (min) => minLength = min;
+RandomWikiquote.setMaxLength = (max) => maxLength = max;
+RandomWikiquote.setNumericLimit = (percentage) => numericLimit = percentage;
+
 const ajaxGet = (url) => {
   return new Promise((resolve, reject) => {
     const xmlhttp = new XMLHttpRequest();
@@ -26,165 +38,151 @@ const ajaxGet = (url) => {
   });
 }
 
-const WikiquoteApi = (() => {
-  var wqa = {};
 
-  const API_URL = "https://en.wikiquote.org/w/api.php?origin=*&format=json";
-  const retryLimit = 7;
+/**
+ * Get all quotes for a given section.
+ *
+ * Returns the titles that were used in case there is a redirect.
+ */
+const getQuotesForSection = async (pageId, sectionIndex) => {
+  const url = BASE_URL + "&action=parse&noimages=&pageid=" + pageId + "&section=" + sectionIndex;
+  const childrenToKeep = ['A', 'B', 'I', 'STRONG', 'EM', 'MARK', 'ABBR', 'SMALL', 'DEL', 'INS', 'SUB', 'SUP', 'PRE', 'CODE', 'DFN', 'SAMP'];
 
-  var minLength = 20;
-  var maxLength = 300;
-  var numericLimit = 0.1;
+  try {
+    const data = await ajaxGet(url);
+    if (!data.parse) // Some pages have no valid sections.
+      throw new Error("Page has no valid section");
 
-  wqa.setMinLength = (min) => minLength = min;
-  wqa.setMaxLength = (max) => maxLength = max;
-  wqa.setNumericLimit = (percentage) => numericLimit = percentage;
+    var quotes = data.parse.text["*"];
 
-  /**
-   * Get all quotes for a given section.
-   *
-   * Returns the titles that were used in case there is a redirect.
-   */
-  wqa.getQuotesForSection = async (pageId, sectionIndex) => {
-    const url = API_URL + "&action=parse&noimages=&pageid=" + pageId + "&section=" + sectionIndex;
-    const childrenToKeep = ['A', 'B', 'I', 'STRONG', 'EM', 'MARK', 'ABBR', 'SMALL', 'DEL', 'INS', 'SUB', 'SUP', 'PRE', 'CODE', 'DFN', 'SAMP'];
+    const parser = new DOMParser();
+    const html = parser.parseFromString(quotes, 'text/html');
+    const allQuotes = html.querySelectorAll('div > ul > li');
 
-    try {
-      const data = await ajaxGet(url);
-      if (!data.parse) // Some pages have no valid sections.
-        throw new Error("Page has no valid section");
+    let parsedQuotes = []
 
-      var quotes = data.parse.text["*"];
+    for (let quote of allQuotes) {
+      let children = Array.from(quote.children); // Convert from live collection to array.
 
-      const parser = new DOMParser();
-      const html = parser.parseFromString(quotes, 'text/html');
-      const allQuotes = html.querySelectorAll('div > ul > li');
+      for (let child of children) // Replace unwanted elements with spaces.
+        if (!childrenToKeep.includes(child.tagName))
+          quote.replaceChild(document.createTextNode(" "), child);
 
-      let parsedQuotes = []
-
-      for (let quote of allQuotes) {
-        // Convert from live collection to array.
-        let children = Array.from(quote.children);
-
-        // replace unwanted elements with spaces.
-        for (let child of children)
-          if (!childrenToKeep.includes(child.tagName))
-            quote.replaceChild(document.createTextNode(" "), child);
-
-        parsedQuotes.push(quote.outerText);
-      }
-
-      return { titles: data.parse.title, quotes: parsedQuotes };
+      parsedQuotes.push(quote.outerText);
     }
-    catch (error) {
-      throw new Error(error);
-    }
+
+    // TODO: Filter out bad quotes here to reduce API calls.
+
+    return { titles: data.parse.title, quotes: parsedQuotes };
   }
-
-  /**
-  * Get the sections for a given page.
-  * This makes parsing for quotes more manageable.
-  * Returns an array of all "1.x" sections as these usually contain the quotes.
-  * If no 1.x sections exists, returns section 1. Returns the titles that were used
-  * in case there is a redirect.
-  */
-  wqa.getSectionsForPage = async (pageId) => {
-    const url = API_URL + "&action=parse&prop=sections&pageid=" + pageId;
-
-    try {
-      const data = await ajaxGet(url);
-      const sectionArray = [];
-      const sections = data.parse.sections;
-      for (let s in sections) {
-        let splitNum = sections[s].number.split('.');
-        if (splitNum.length > 1 && splitNum[0] === "1") {
-          sectionArray.push(sections[s].index);
-        }
-      }
-      // Use section 1 if there are no "1.x" sections
-      if (sectionArray.length === 0) {
-        sectionArray.push("1");
-      }
-      return { pageId: pageId, titles: data.parse.title, sections: sectionArray };
-    }
-    catch (error) {
-      throw new Error(error);
-    }
+  catch (error) {
+    throw new Error(error);
   }
+}
 
-  /**
-   * Gets a random page id from the main namespace.
-   */
-  wqa.getRandomPage = async () => {
-    const url = API_URL + "&action=query&list=random&rnnamespace=0&rnlimit=1";
+/**
+* Get the sections for a given page.
+* This makes parsing for quotes more manageable.
+* Returns an array of all "1.x" sections as these usually contain the quotes.
+* If no 1.x sections exists, returns section 1. Returns the titles that were used
+* in case there is a redirect.
+*/
+const getSectionsForPage = async (pageId) => {
+  const url = BASE_URL + "&action=parse&prop=sections&pageid=" + pageId;
 
-    try {
-      const data = await ajaxGet(url);
-      const id = data.query.random[0].id;
-      if (!id) throw new Error("Invalid random page id");
-      return id;
-    }
-    catch (error) {
-      throw new Error(error);
-    }
-  }
-
-  /**
-   * Gets a random quote from a random title in the main namespace
-   * Quotes will either not be found or rejected for various reasons
-   * Keeps trying for a valid quote until retryLimit is reached
-   */
-  wqa.getRandomQuote = () => {
-    return new Promise((resolve, reject) => {
-      var numRetry = 0;
-
-      var randomNum = max => Math.floor(Math.random() * max);
-      var randomSection = sections => sections[randomNum(sections.length)];
-      var randomQuote = quotes => quotes.quotes[randomNum(quotes.quotes.length)];
-      var chooseQuote = quotes => ({ title: quotes.titles, quote: randomQuote(quotes) });
-
-      var checkQuote = quote => {
-        if (!quote.quote) // Some pages have title and sections but no quotes
-          return Promise.reject('No quote found');
-
-        var length = quote.quote.length;
-        var digitPercentage = (quote.quote.match(/\d/g) || []).length / length;
-
-        if (length < minLength)
-          return Promise.reject("Quote is too short");
-        else if (length > maxLength)
-          return Promise.reject("Quote is too long");
-        else if (digitPercentage > numericLimit)  // Some quotes are just dates and times
-          return Promise.reject("Quote is too numeric");
-        else
-          return quote;
-      };
-
-      var checkRetry = (reason) => {
-        console.log(reason + ". Retrying...");
-        numRetry++;
-
-        if (numRetry >= retryLimit)
-          reject("Retry limit reached");
-        else
-          mainSequence();
+  try {
+    const data = await ajaxGet(url);
+    const sectionArray = [];
+    const sections = data.parse.sections;
+    for (let s in sections) {
+      let splitNum = sections[s].number.split('.');
+      if (splitNum.length > 1 && splitNum[0] === "1") {
+        sectionArray.push(sections[s].index);
       }
+    }
+    // Use section 1 if there are no "1.x" sections
+    if (sectionArray.length === 0) {
+      sectionArray.push("1");
+    }
+    return { pageId: pageId, titles: data.parse.title, sections: sectionArray };
+  }
+  catch (error) {
+    throw new Error(error);
+  }
+}
 
-      var mainSequence = () => {
-        wqa.getRandomPage()
-          .then(pageId => wqa.getSectionsForPage(pageId))
-          .then(data => wqa.getQuotesForSection(data.pageId, randomSection(data.sections)))
-          .then(quotes => chooseQuote(quotes))
-          .then(quote => checkQuote(quote))
-          .then(theQuote => resolve(theQuote))
-          .catch(reason => checkRetry(reason));
-      };
+/**
+ * Gets a random page id from the main namespace.
+ */
+const getRandomPage = async () => {
+  const url = BASE_URL + "&action=query&list=random&rnnamespace=0&rnlimit=1";
 
-      mainSequence();
-    });
-  };
+  try {
+    const data = await ajaxGet(url);
+    const id = data.query.random[0].id;
+    if (!id) throw new Error("Invalid random page id");
+    return id;
+  }
+  catch (error) {
+    throw new Error(error);
+  }
+}
 
-  return wqa;
-})();
+/**
+ * Gets a random quote from a random title in the main namespace
+ * Quotes will either not be found or rejected for various reasons
+ * Keeps trying for a valid quote until RETRY_LIMIT is reached
+ */
+const getRandomQuote = () => {
+  return new Promise((resolve, reject) => {
+    var numRetry = 0;
 
-export default WikiquoteApi;
+    var randomNum = max => Math.floor(Math.random() * max);
+    var randomSection = sections => sections[randomNum(sections.length)];
+    var randomQuote = quotes => quotes.quotes[randomNum(quotes.quotes.length)];
+    var chooseQuote = quotes => ({ title: quotes.titles, quote: randomQuote(quotes) });
+
+    var checkQuote = quote => {
+      if (!quote.quote) // Some pages have title and sections but no quotes
+        return Promise.reject('No quote found');
+
+      var length = quote.quote.length;
+      var digitPercentage = (quote.quote.match(/\d/g) || []).length / length;
+
+      if (length < minLength)
+        return Promise.reject("Quote is too short");
+      else if (length > maxLength)
+        return Promise.reject("Quote is too long");
+      else if (digitPercentage > numericLimit)  // Some quotes are just dates and times
+        return Promise.reject("Quote is too numeric");
+      else
+        return quote;
+    };
+
+    var checkRetry = (reason) => {
+      console.log(reason + ". Retrying...");
+      numRetry++;
+
+      if (numRetry >= RETRY_LIMIT)
+        reject("Retry limit reached");
+      else
+        mainSequence();
+    }
+
+    var mainSequence = () => {
+      getRandomPage()
+        .then(pageId => getSectionsForPage(pageId))
+        .then(data => getQuotesForSection(data.pageId, randomSection(data.sections)))
+        .then(quotes => chooseQuote(quotes))
+        .then(quote => checkQuote(quote))
+        .then(theQuote => resolve(theQuote))
+        .catch(reason => checkRetry(reason));
+    };
+
+    mainSequence();
+  });
+};
+
+RandomWikiquote.getRandomQuote = getRandomQuote;
+
+export default RandomWikiquote;
